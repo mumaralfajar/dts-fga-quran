deb38d9b10f80feb91591209095a6deb6a2bcc0a
mumaralfajar <kuhaaku@gmail.com>
2022-06-21 16:42:30 +0700

    add animations & styles change

--- js/script.js
+++ js/script.js
@@ -1,59 +1,78 @@
 const btn = document.getElementById('btn');
 
-function getAyah() {
+function getAyah(callback) {
     fetch("https://api.alquran.cloud/v1/ayah/random/ar")
-    .then(response => response.json())
-    .then(data => {
-        let number = data["data"]["number"]
-        let ayat = data["data"]["text"]
-        let surah = data["data"]["surah"]["englishName"]
-        let noAyah = data["data"]["numberInSurah"]
-        str = `<label>Surah: ${surah}</label> <label>Ayat: ${noAyah}</label>`
-        document.getElementById("but").innerHTML = str;
-        document.getElementById("ayah").innerHTML = ayat;
-        console.log(data["data"]["surah"]["englishName"])
-        console.log(data["data"]["surah"]["number"])
-
-        fetch(`https://api.alquran.cloud/v1/ayah/${number}/id.indonesian`)
         .then(response => response.json())
         .then(data => {
-            let arti = data["data"]["text"]
-            document.getElementById("meaning").innerHTML = arti;
-            console.log(data["data"]["text"])
-        });
+            let number = data["data"]["number"]
+            let ayat = data["data"]["text"]
+            let surah = data["data"]["surah"]["englishName"]
+            let noAyah = data["data"]["numberInSurah"]
+            str = `<label class="glass-label">Surah: ${surah}</label> <label class="glass-label">Ayat: ${noAyah}</label>`
+            document.getElementById("but").innerHTML = str;
+            document.getElementById("ayah").innerHTML = ayat;
+            console.log(data["data"]["surah"]["englishName"])
+            console.log(data["data"]["surah"]["number"])
+
+        fetch(`https://api.alquran.cloud/v1/ayah/${number}/id.indonesian`)
+            .then(response => response.json())
+            .then(data => {
+                let arti = data["data"]["text"]
+                document.getElementById("meaning").innerHTML = arti;
+                console.log(data["data"]["text"])
+            });
     });
+    callback();
 }
 
-getBtn.addEventListener('click', function() {
-    getAyah();
-})
+function masok() {
+    $( "main" ).fadeIn( "slow");
+}
 
-window.onload = function() {
+getBtn.addEventListener('click', function () {
+    // if (isFilled = true) {
+    //     $( "main" ).fadeOut( "slow", function() {
+    //         $( "main" ).fadeIn( "slow", function() {
+    //             getAyah();
+    //         });
+    //     });
+    // } else {
+    //     $( "main" ).fadeIn( "slow", function() {
+    //         getAyah();
+    //     });
+    // }
+    $( "main" ).fadeOut( "slow", function() {
+        getAyah(masok);
+    });
+});
+
+window.onload = function () {
     Particles.init({
-    selector: '.background',
-    color: 'green',
-    maxParticles: 300,
-      
-    // options for breakpoints
-    responsive: [
-        {
-            breakpoint: 768,
-            options: {
-                maxParticles: 200,
-                color: '#48F2E3',
-                connectParticles: false
+        selector: '.background',
+        color: 'green',
+        maxParticles: 300,
+
+        // options for breakpoints
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 200,
+                    color: '#48F2E3',
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 100,
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                }
             }
-        }, {
-            breakpoint: 425,
-            options: {
-                maxParticles: 100,
-                connectParticles: false
-            }
-        }, {
-          breakpoint: 320,
-          options: {
-            maxParticles: 0
-          }
-        }
-    ]
-})};
+        ]
+    })
+};
